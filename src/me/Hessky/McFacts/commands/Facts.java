32c8e46ef57c7fff766c5b47ac7f9ffde3d5ea5a
Hessky <51374719+HesskyDev@users.noreply.github.com>
2025-03-05 12:34:43 +0100

    Updated Facts.java
    Using AI cleaned the code

--- src/me/Hessky/McFacts/commands/Facts.java
+++ src/me/Hessky/McFacts/commands/Facts.java
@@ -1,7 +1,5 @@
 package me.Hessky.McFacts.commands;
 
-
-
 import java.util.HashMap;
 import java.util.Map;
 
@@ -15,101 +13,85 @@ import org.bukkit.entity.Player;
 import me.Hessky.McFacts.Main;
 import me.Hessky.McFacts.utils.Utils;
 
+public class Facts implements CommandExecutor {
 
-public class Facts implements CommandExecutor{
-	
-	private Main plugin;
-	
-	public Facts(Main plugin){
-		
-		this.plugin = plugin;
-		
-		plugin.getCommand("Facts").setExecutor(this);
-	}
-	
-	Map<String, Long> cooldown = new HashMap<String, Long>();
-	
-	@Override
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-	
-		//Checking if the sender is a player
-		if(!(sender instanceof Player)) {
-			
-			//Only a player can execute the cmd warning
-			sender.sendMessage(Utils.chat(plugin.getConfig().getString("consolewarn")));
-			
-			return true;
-		}
-			
-			Player p = (Player) sender;
-			//Location for playSound
-			Location ploc = p.getLocation();
-			
-			//COOLDOWN
-			
-			if(cooldown.containsKey(p.getName())) {
-				
-				if(cooldown.get(p.getName()) > System.currentTimeMillis()) {
-					
-					long timeleft = (cooldown.get(p.getName()) - System.currentTimeMillis()) / 1000;
-					p.sendMessage(Utils.chat(plugin.getConfig().getString("prefix") + " &4Coolodwn " + timeleft + " &4second(s)."));
-					
-					return true;
-				}
-			}
-			
-			if(!(p.hasPermission("op") || p.hasPermission("mcfacts.cooldown"))) {
-				cooldown.put(p.getName(), System.currentTimeMillis()+ plugin.getConfig().getInt("cooldown") * 1000);
-			}
-			
-			//Facts
-			if(plugin.getConfig().getString("chatmessage").toLowerCase().contains("true")) {
-				
-				//Rare facts
-				int randomnum = (int) (Math.random()*1000);
-				
-				if(randomnum <= plugin.getConfig().getInt("legendarychance")) {
-					
-					p.sendTitle(Utils.chat(plugin.getConfig().getString("legendarycolor") + "Legendary Fact"), "", 30, 70, 20);
-					p.playSound(ploc, Sound.BLOCK_NOTE_BLOCK_BELL, 3.0F, 0.5F);
-					p.sendMessage(Utils.chat(plugin.getConfig().getString("prefix") + plugin.getConfig().getString("legendarycolor") + plugin.getConfig().getList("legendaryfacts").get((int)(Math.random()*plugin.getConfig().getList("legendaryfacts").size()))));
-					
-					return true;
-				
-				}else {
-					
-					//Epic fact
-					if(randomnum <= plugin.getConfig().getInt("epicchance")  + plugin.getConfig().getInt("legendarychance")) {
-						
-						p.sendTitle(Utils.chat(plugin.getConfig().getString("epiccolor") + "Epic Fact"), "", 30, 70, 20);
-						p.playSound(ploc, Sound.BLOCK_NOTE_BLOCK_BELL, 3.0F, 0.5F);
-						p.sendMessage(Utils.chat(plugin.getConfig().getString("prefix") + plugin.getConfig().getString("epiccolor") + plugin.getConfig().getList("epicfacts").get((int)(Math.random()*plugin.getConfig().getList("epicfacts").size()))));
-						
-						return true;
-					
-					}else {
-						
-						//Rare fact
-						if(randomnum <= plugin.getConfig().getInt("rarechance") + plugin.getConfig().getInt("epicchance")){
-							
-							p.sendTitle(Utils.chat(plugin.getConfig().getString("rarecolor") + "Rare Fact"), "",30, 70, 20);
-							p.playSound(ploc, Sound.BLOCK_NOTE_BLOCK_BELL, 3.0F, 0.5F);
-							p.sendMessage(Utils.chat(plugin.getConfig().getString("prefix") + plugin.getConfig().getString("rarecolor") + plugin.getConfig().getList("rarefacts").get((int)(Math.random()*plugin.getConfig().getList("rarefacts").size()))));
-							
-							return true;
-						
-						}else {
-							
-							//Normal Fact
-							p.playSound(ploc, Sound.BLOCK_NOTE_BLOCK_BELL, 3.0F, 0.5F);
-							p.sendMessage(Utils.chat(plugin.getConfig().getString("prefix") + plugin.getConfig().getString("commoncolor") +plugin.getConfig().getList("commonfacts").get((int)(Math.random()*plugin.getConfig().getList("commonfacts").size()))));
-						}
-					}
-				}
-					
-			}
-			
-			return false;
-	
-	}
+    private final Main plugin;
+    private final Map<String, Long> cooldown = new HashMap<>();
+
+    public Facts(Main plugin) {
+        this.plugin = plugin;
+        plugin.getCommand("Facts").setExecutor(this);
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage(Utils.chat(plugin.getConfig().getString("consolewarn")));
+            return true;
+        }
+
+        Player player = (Player) sender;
+        Location playerLocation = player.getLocation();
+
+        if (!checkCooldown(player)) {
+            return true;
+        }
+
+        if (plugin.getConfig().getString("chatmessage").equalsIgnoreCase("true")) {
+            sendRandomFact(player, playerLocation);
+        }
+
+        return true;
+    }
+
+    private boolean checkCooldown(Player player) {
+        long currentTime = System.currentTimeMillis();
+        if (cooldown.containsKey(player.getName())) {
+            long timeLeft = (cooldown.get(player.getName()) - currentTime) / 1000;
+            if (timeLeft > 0) {
+                player.sendMessage(Utils.chat(plugin.getConfig().getString("prefix") + " &4Cooldown: " + timeLeft + " second(s)."));
+                return false;
+            }
+        }
+
+        if (!(player.hasPermission("op") || player.hasPermission("mcfacts.cooldown"))) {
+            cooldown.put(player.getName(), currentTime + plugin.getConfig().getInt("cooldown") * 1000);
+        }
+        return true;
+    }
+
+    private void sendRandomFact(Player player, Location location) {
+        int randomNum = (int) (Math.random() * 1000);
+
+        if (sendFact(player, location, "legendary", randomNum <= plugin.getConfig().getInt("legendarychance"))) return;
+        if (sendFact(player, location, "epic", randomNum <= plugin.getConfig().getInt("epicchance") + plugin.getConfig().getInt("legendarychance"))) return;
+        if (sendFact(player, location, "rare", randomNum <= plugin.getConfig().getInt("rarechance") + plugin.getConfig().getInt("epicchance"))) return;
+
+        // Common fact as fallback
+        sendFactMessage(player, "commoncolor", "commonfacts");
+        playSound(location);
+    }
+
+    private boolean sendFact(Player player, Location location, String rarity, boolean condition) {
+        if (condition) {
+            player.sendTitle(Utils.chat(plugin.getConfig().getString(rarity + "color") + capitalize(rarity) + " Fact"), "", 30, 70, 20);
+            sendFactMessage(player, rarity + "color", rarity + "facts");
+            playSound(location);
+            return true;
+        }
+        return false;
+    }
+
+    private void sendFactMessage(Player player, String colorKey, String factKey) {
+        player.sendMessage(Utils.chat(plugin.getConfig().getString("prefix") + plugin.getConfig().getString(colorKey)
+                + plugin.getConfig().getList(factKey).get((int) (Math.random() * plugin.getConfig().getList(factKey).size()))));
+    }
+
+    private void playSound(Location location) {
+        location.getWorld().playSound(location, Sound.BLOCK_NOTE_BLOCK_BELL, 3.0F, 0.5F);
+    }
+
+    private String capitalize(String str) {
+        return str.substring(0, 1).toUpperCase() + str.substring(1);
+    }
 }
